62878badaa48522d44e881423cb56df8eab0869c
AkshayGhoshTR <AkshayGhosh30@gmail.com>
2025-09-25 21:42:21 +0530

    Updated the main.tf file with aws-auth config map creation and same in the workflow as well

--- terraform-project/main.tf
+++ terraform-project/main.tf
@@ -26,13 +26,72 @@ module "vpc" {
 module "eks" {
   source = "./modules/eks"
 
-  project_name         = var.project_name
-  cluster_version      = var.cluster_version
-  private_subnet_ids   = module.vpc.private_subnet_ids
-  instance_types=      var.instance_types
+  project_name       = var.project_name
+  cluster_version    = var.cluster_version
+  private_subnet_ids = module.vpc.private_subnet_ids
+  instance_types     = var.instance_types
   
   # Node group scaling
-  desired_size         = 2
-  max_size             = 3
-  min_size             = 1
+  desired_size       = 2
+  max_size           = 3
+  min_size           = 1
+}
+
+
+#---------- Change after the LOCAL Access Issue ----------------
+#ISSUE : When bootstraping the IAM User in the EKS Cluster, ITS IDENTITY IS CREATED INSIDE THE CLUSTER [with master acces]
+# For GitHub IAM User --> Since Bootstraped, this User has both Authentication and authorization
+                          # It can talk the EKs API and has RBAC permissions in K8
+
+# For the Local IAM User --> EKs allows authentication via the IAM [Hence, we did not get issue while updating kubeconfig]
+                            # Till here aws knew that we are the same IAM user, AUTHENTICATEd
+                            # PROBLEM IS HERE : This user is not mapped in the aws auth configMap 
+                            
+                            
+# Means,Bootstraping[concept tried earlier] --> AUTOMATICALLY GIVES THE CLUSTER CREATOR IAM ENTITY RBAC RIGHTs
+        # Therefore the IAM User [whoever created the Cluster] was never added to the aws-auth ConfigMap
+        # So authentication was passing from the AWS side
+        # Authorization was failing from the RBAC side because IAM user was never added to the aws-auth ConfigMap
+
+#---------------------------------------------------------------------------------------------------------------------------------------------
+# SECTION : To add the IAM User to the aws-auth ConfigMap, 
+# --------------------> granting it admin privileges.
+
+#  Using here "kubernetes_config_map_v1" to ensure that it safely merges the changes without overwriting the entire ConfigMap
+resource "kubernetes_config_map_v1" "aws_auth" {
+  metadata {
+    name      = "aws-auth"
+    namespace = "kube-system"
+  }
+
+  data = {
+  # creates a mapping in the aws-auth ConfigMap that tells Kubernetes 
+  #how to handle requests from the EC2 Ins (worker nodes)
+
+ 
+    "mapRoles" = yamlencode([
+      {
+        rolearn  = module.eks.node_role_arn
+        username = "system:node:{{EC2PrivateDNSName}}"
+        groups = [
+          "system:bootstrappers",
+          "system:nodes",
+        ]
+      },
+    ])
+
+    # admin user, giving  access to the IAM user.
+    "mapUsers" = yamlencode([
+      {
+        # MAIN LOGIC: Taking the IAM User ARN on the flow and splitting the username from it after "/"
+        userarn  = var.cluster_creator_arn
+        username = split("/", var.cluster_creator_arn)[1]
+        groups   = ["system:masters"]
+      },
+    ])
+  }
+
+
+# Making sure that the eks cluster is created
+  depends_on = [module.eks]
 }
